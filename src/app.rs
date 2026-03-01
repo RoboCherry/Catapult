310ea91ddc50a151909fab0e93c120cc11180576
RoboCherry <150583047+RoboCherry@users.noreply.github.com>
2026-03-01 23:08:43 +0000

    Added app grouping
    hella buggy, this is just a commit not to lose my work yk how it is

--- src/app.rs
+++ src/app.rs
@@ -5,9 +5,9 @@ use egui::{Color32, ColorImage, FontFamily, FontId, Id, Key, RichText, TextStyle
 use rfd::FileDialog;
 use sysinfo::{Pid, Process, ProcessRefreshKind, RefreshKind, System};
 
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
+
 #[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
+#[serde(default)]
 pub struct CatapultApp {
 
     #[serde(skip)]
@@ -15,13 +15,14 @@ pub struct CatapultApp {
     #[serde(skip)]
     last_instant : Instant,
 
+    app_folders : HashMap<String,Vec<String>>,
     apps : Vec<String>,
     selected_app : String,
     apps_aliases : HashMap<String, String>,
     app_play_time : HashMap<String, u64>,
 
     #[serde(skip)]
-    app_texture_handles : HashMap<String, TextureHandle>,
+    app_texture_handles : HashMap<String, TextureHandle>, //Cache the texture handles for the app icons, so we don't have to reload them every frame (big performance increase trust me, i wish i could serialize texture handles but alas)
 
     #[serde(skip)]
     edit : bool,
@@ -29,15 +30,19 @@ pub struct CatapultApp {
     #[serde(skip)]
     is_app_selected : bool,
     #[serde(skip)]
+    is_folder_created : bool,
+    #[serde(skip)]
     current_app_name : String,
     #[serde(skip)]
     current_path : String,
+    #[serde(skip)]
+    current_folder_name : String,
 
     #[serde(skip)]
     sys : System,
 
     #[serde(skip)]
-    running_apps : HashMap<String,usize>,
+    running_apps : HashMap<String,usize>, //Stores the PID of all running apps, so we can track it and update play time
     #[serde(skip)]
     app_to_remove : String,
 }
@@ -47,6 +52,7 @@ impl Default for CatapultApp {
         Self {
             delta_time : Duration::new(0, 0),
             last_instant : Instant::now(),
+            app_folders : HashMap::new(),
             apps : Vec::new(),
             apps_aliases : HashMap::new(),
             app_texture_handles : HashMap::new(),
@@ -54,8 +60,10 @@ impl Default for CatapultApp {
             selected_app : "".to_string(),
             edit : false,
             is_app_selected : false,
+            is_folder_created : false,
             current_app_name : "".to_string(),
             current_path : "".to_string(),
+            current_folder_name : "".to_string(),
             sys : System::new_with_specifics(RefreshKind::nothing().with_processes(ProcessRefreshKind::everything())),
             running_apps : HashMap::new(),
             app_to_remove : "".to_string(),
@@ -75,7 +83,6 @@ impl CatapultApp {
 }
 
 impl eframe::App for CatapultApp {
-    /// Called by the framework to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
@@ -168,6 +175,19 @@ impl eframe::App for CatapultApp {
                     };
                 }); 
             }
+            if self.is_folder_created{
+                Window::new("Create Folder").show(ctx, |ui|{
+                    ui.add(egui::TextEdit::singleline(&mut self.current_folder_name).hint_text("New Group").min_size(Vec2 { x: 512.0, y: 0.0 }));
+                    if ui.button("Add Group").clicked() || ui.input(|i| i.key_pressed(Key::Enter)){
+                        self.app_folders.insert(self.current_folder_name.clone(), Vec::new());
+                        self.current_folder_name = "".to_string();
+                        self.is_folder_created = false;
+                    }
+                    if ui.button("Cancel").clicked() || ui.input(|i| i.key_pressed(Key::Escape)){
+                        self.is_folder_created = false
+                    };
+                });
+            }
 
             
             ui.heading("Applications");
@@ -181,7 +201,6 @@ impl eframe::App for CatapultApp {
             if ui.button("Add App [+]").clicked() {
 
                 let files = FileDialog::new()
-                //.add_filter("Executable", &["exe"])
                 .set_directory("C:/")
                 .pick_file();
                 
@@ -193,39 +212,80 @@ impl eframe::App for CatapultApp {
                     self.is_app_selected = true;
                 }
             };
+            if ui.button("Add Group [+]").clicked() {
+                self.is_folder_created = true;
+            }
             ui.add_space(16.0);
             
             egui::ScrollArea::vertical().show(ui, |ui| {
-                for i in &mut self.apps.iter(){
+                ui.menu_button("ALL Apps", |ui| {
+                    egui::ScrollArea::vertical().show(ui, |ui| {
+                        for i in &mut self.apps.iter(){
 
-                    let sized_image : SizedTexture;
+                        let sized_image : SizedTexture;
 
-                    if self.app_texture_handles.get(&i.to_string()).is_none(){
-                        let color_icon = get_color_icon(i.clone(), [128,128]);
-                        let handle = ctx.load_texture("app_icon", color_icon.clone(), TextureOptions::LINEAR);
-                        sized_image = egui::load::SizedTexture::new(handle.id(), egui::vec2(48.0, 48.0));
-                        self.app_texture_handles.insert(i.clone(), handle.clone());
-                    } else {
-                        let handle = self.app_texture_handles.get(&i.to_string()).unwrap();
-                        sized_image = egui::load::SizedTexture::new(handle.id(), egui::vec2(48.0, 48.0));
+                        if self.app_texture_handles.get(&i.to_string()).is_none(){
+                            let color_icon = get_color_icon(i.clone(), [128,128]);
+                            let handle = ctx.load_texture("app_icon", color_icon.clone(), TextureOptions::LINEAR);
+                            sized_image = egui::load::SizedTexture::new(handle.id(), egui::vec2(48.0, 48.0));
+                            self.app_texture_handles.insert(i.clone(), handle.clone());
+                        } else {
+                            let handle = self.app_texture_handles.get(&i.to_string()).unwrap();
+                            sized_image = egui::load::SizedTexture::new(handle.id(), egui::vec2(48.0, 48.0));
+                        }
+                        let icon = egui::Image::from_texture(sized_image);
+
+                        let app_name = self.apps_aliases.get(i);
+                        let text : RichText;
+                        if app_name.is_some(){
+                            text = RichText::new(app_name.unwrap().to_string()).size(24.0);
+                        } else {
+                            text = RichText::new(i).size(24.0);
+                        }
+
+                        if ui.add(egui::Button::image_and_text(icon.clone(), text.clone()).min_size(Vec2 { x: 32.0, y: 32.0 })).clicked(){
+                            self.selected_app = i.to_string();
+                        }
+                        ui.add_space(8.0);
                     }
-                    let icon = egui::Image::from_texture(sized_image);
-
-                    let app_name = self.apps_aliases.get(i);
-                    let text : RichText;
-                    if app_name.is_some(){
-                        text = RichText::new(app_name.unwrap().to_string()).size(24.0);
-                    } else {
-                        text = RichText::new(i).size(24.0);
-                    }
-
-                    if ui.add(egui::Button::image_and_text(icon.clone(), text.clone()).min_size(Vec2 { x: 32.0, y: 32.0 })).clicked(){
-                        self.selected_app = i.to_string();
-                    }
-                    ui.add_space(8.0);
-                }
+                });   
             });
+            ui.add_space(24.0);
+            for folder in self.app_folders.keys(){
+                ui.menu_button(folder, |ui| {
+                    egui::ScrollArea::vertical().show(ui, |ui| {
+                        for app in self.app_folders.get(folder).unwrap(){
+                            let sized_image : SizedTexture;
+
+                            if self.app_texture_handles.get(&app.to_string()).is_none(){
+                                let color_icon = get_color_icon(app.clone(), [128,128]);
+                                let handle = ctx.load_texture("app_icon", color_icon.clone(), TextureOptions::LINEAR);
+                                sized_image = egui::load::SizedTexture::new(handle.id(), egui::vec2(48.0, 48.0));
+                                self.app_texture_handles.insert(app.clone(), handle.clone());
+                            } else {
+                                 let handle = self.app_texture_handles.get(&app.to_string()).unwrap();
+                                sized_image = egui::load::SizedTexture::new(handle.id(), egui::vec2(48.0, 48.0));
+                            }
+                            let icon = egui::Image::from_texture(sized_image);
+
+                            let app_name = self.apps_aliases.get(app);
+                            let text : RichText;
+                            if app_name.is_some(){
+                                text = RichText::new(app_name.unwrap().to_string()).size(24.0);
+                            } else {
+                                text = RichText::new(app).size(24.0);
+                            }
+
+                            if ui.add(egui::Button::image_and_text(icon.clone(), text.clone()).min_size(Vec2 { x: 32.0, y: 32.0 })).clicked(){
+                                self.selected_app = app.to_string();
+                            }
+                            ui.add_space(8.0);
+                        }
+                    });
+                });
+            }
         });
+    });
 
         egui::CentralPanel::default()
             .show(ctx, |ui|{
@@ -248,6 +308,17 @@ impl eframe::App for CatapultApp {
                     if ui.add(egui::Button::new("Edit App")).clicked(){
                         self.edit = true;
                     }
+                    ui.menu_button("Add to Group", |ui|{
+                        let folder_names: Vec<String> = self.app_folders.keys().cloned().collect();
+                        for folder in folder_names{
+                            if ui.button(&folder).clicked(){
+                                let folder_vec = self.app_folders.get_mut(&folder).unwrap();
+                                if !folder_vec.contains(&self.selected_app.clone()){
+                                    folder_vec.push(self.selected_app.clone());
+                                }
+                            }
+                        }
+                    });
                     let readable_time = time_from_millis(*self.app_play_time.get(&self.selected_app).unwrap_or(&(0 as u64)));
                     ui.label(format!("Time played: {}", readable_time));
                 } else {
@@ -327,14 +398,14 @@ impl eframe::App for CatapultApp {
 }
 
 
-fn get_executable_name(path : &String) -> String{
+fn get_executable_name(path : &String) -> String{ //Gets the name of an executable from its path, to use as the default app name if the user doesn't specify one. For example, "C:\Program Files\Example\example.exe", will return "example"
     let mut new_path = path.clone();
     let split_path : Vec<&str> = new_path.split("\\").collect();
     new_path = (*split_path.get(split_path.iter().count() - 1).unwrap()).to_string().replace(".exe", "");
     new_path
 }
 
-fn set_stylings(ctx: &egui::Context){
+fn set_stylings(ctx: &egui::Context){ //Makes egui look pretty
     let mut style = (*ctx.style()).clone();
     style.text_styles = [
         (TextStyle::Heading, FontId::new(18.0, FontFamily::Monospace)),
@@ -365,7 +436,7 @@ fn set_stylings(ctx: &egui::Context){
     ctx.set_theme(ThemePreference::Dark);
 }
 
-fn open_app(name : &String) -> usize{
+fn open_app(name : &String) -> usize{ //Given an executable path, open the executable and return its PID so we can track it with track_app. If the app fails to open, return 0 (which will never be a valid PID)
     let mut command = Command::new(name);
     
     if let Ok(child) = command.spawn() {
@@ -376,12 +447,12 @@ fn open_app(name : &String) -> usize{
     }
 }
 
-fn track_app(pid : usize, app : &CatapultApp) -> Option<&Process>{
+fn track_app(pid : usize, app : &CatapultApp) -> Option<&Process>{ //Given a PID, return the corresponding Process struct from sysinfo, which we can use to track if the app is still running and get other info if we want
     let process = app.sys.process(Pid::from(pid));
     process
 }
 
-fn get_color_icon(exe_path : String, size : [usize; 2]) -> ColorImage{
+fn get_color_icon(exe_path : String, size : [usize; 2]) -> ColorImage{ //Loads the file icon for the given executable path and converts it to an egui ColorImage, which can then be loaded as a texture and displayed in the UI
     let app_icon = get_file_icon(exe_path.clone(), 128).expect("Failed to get icon");
     let app_icon_image = RgbaImage::from_raw(app_icon.width, app_icon.height, app_icon.pixels)
         .map(DynamicImage::ImageRgba8)
